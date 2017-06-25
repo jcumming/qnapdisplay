778fb22326d24304f44fc962f584bea73d7f1509
Jack Cummings <jack@mudshark.org>
2017-06-24 21:53:51 -0700

    make us a module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='qnapdisplay',
+      version='0.1',
+      description='Library for interacting with front-panel displays on qnap products',
+      url='http://github.com/jcumming/qnapdisplay',
+      author='Mark de Bruijn',
+      license='GPL2',
+      packages=['qnapdisplay'],
+      zip_safe=False)
